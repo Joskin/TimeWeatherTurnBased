467fe615014748b4d6334643895e8c24195914f6
Joskin <zabka.josquin@gmail.com>
2015-08-07 23:06:36 -0400

    Update Events.cs

--- Assets/Internal/Code/Events.cs
+++ Assets/Internal/Code/Events.cs
@@ -8,37 +8,6 @@ using System.Collections.Generic;
 // - un EventHandler static pour ajouter/supprimer les events
 // - une fonction Send pour envoyer les events
 
-/*
-public class PlayerEvent
-{
-	public delegate void PlayerEventHandler(PlayerEvent _event);
-	static public event PlayerEventHandler EventHandler;
-	static public void Send(PlayerEvent _event) { EventHandler.Invoke(_event); }
-	
-	//--------------------------------------------------------------------------------------------------
-	
-	public enum Type
-	{
-		Stun_Start,			// Un Joueur vient d'etre Stun
-		Stun_End,			// Un Joueur vient de sortir du Stun
-	}
-	
-	//--------------------------------------------------------------------------------------------------
-	
-	public Type type { get { return this.mType; } }
-	public Player player { get { return this.mPlayer; } }
-	
-	private Type mType;
-	private Player mPlayer;
-	
-	public PlayerEvent(Type _type, Player _player)
-	{
-		this.mType = _type;
-		this.mPlayer = _player;
-	}
-}
-*/
-//================================================================================================================================
 
 public class TurnEvent
 {
